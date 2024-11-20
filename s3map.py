88e95d6c86db503c1237f09a9be313a512b6f16d
JialeCheng100 <jialecheng100@gmail.com>
2024-11-20 16:36:57 -0500

    Added new pre-trained models

--- s3map.py
+++ s3map.py
@@ -16,7 +16,7 @@ import math
 
 from s3pipe.models.models import SUnet
 from s3pipe.utils.interp_torch import convert2DTo3D, getEn, diffeomorp_torch, get_bi_inter
-from s3pipe.utils.utils import get_neighs_order, get_sphere_template, get_neighs_faces
+from s3pipe.utils.utils import get_neighs_order, get_sphere_template
 from s3pipe.surface.s3map import ResampledInnerSurf, computeMetrics_torch,\
     moveOrigSphe, computeAndWriteDistortionOnOrigSphe, \
         computeAndWriteDistortionOnRespSphe
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                                                               "Either single file or files pattern should be given. "+\
                                                               "Note the pattern needs to be quoted for python otherwise it will be parsed by the shell by default.")
     parser.add_argument('--hemi', help="hemisphere, lh or rh", choices=['lh', 'rh'], required=True)
-    parser.add_argument('--device', default='GPU', choices=['GPU', 'CPU'], 
+    parser.add_argument('--device', default='CPU', choices=['GPU', 'CPU'], 
                         help='The device for running the model.')
     parser.add_argument('--model_path', default=None, help="the folder containing all models, if not given, it will be ./pretrained_models ")
     parser.add_argument('--save_interim_results', default='True', help="save intermediate results or not, if Ture, there will be many results generated and need more storage", 
@@ -79,33 +79,35 @@ if __name__ == "__main__":
     print('files:', files)
     print('len(files):', len(files))
     
-    dataset = 'HighRes'   # 'HighRes' for dataset BCP and HCP with vertices number between 140000 - 250000, 'LowRes' for dataset dHCP with vertices between 30000 to 140000
     weight_distan = 1.0
     weight_area = 0.1
-    weight_smooth = 10.0
+    
     batch_size = 1
     n_vertexs = [10242, 40962, 163842]
+    # n_vertexs = [10242, 40962]
 
     # start distortion correction for each level
     for n_vertex in n_vertexs:
         n_level = n_vertexs.index(n_vertex)
         print('\n------------------------------------------------------------------')
-        print("Starting distortion correction on", n_level+1, "-th level with", n_vertex, "vertices.")
+        print("Starting distortion correction on {}-th level with {} vertices.".format(n_level+1, n_vertex))
     
         if n_vertex == 10242:
-            deform_scale = 10.0
+            
             level = 6
             n_res = 4 
+            weight_smooth = 10.0
             resp_inner_file = files
         elif n_vertex == 40962:
-            deform_scale = 10.0
             level = 7
             n_res = 4 
+            weight_smooth = 5.0
             resp_inner_file = [ x.replace('.SIP.RespInner.vtk', '.SIP.10242moved.RespInner.vtk') for x in files ]
         elif n_vertex == 163842:
-            deform_scale = 20.0
             level = 8
             n_res = 5 
+            # weight_smooth = 2.0
+            weight_smooth = 2.5 # Jiale
             resp_inner_file = [ x.replace('.SIP.RespInner.vtk', '.SIP.10242moved.40962moved.RespInner.vtk') for x in files ]
         else:
             raise NotImplementedError('Error')
@@ -116,10 +118,13 @@ if __name__ == "__main__":
         
         model = SUnet(in_ch=12, out_ch=2, level=level, n_res=n_res, rotated=0, complex_chs=32)
         model.to(device)
-        model_file = os.path.join(model_path, 'S3Map_'+ dataset+'_'+hemi+'_ver'+ str(n_vertex) + '_area'+ str(weight_area) + \
+        model_file = os.path.join(model_path, 'S3Map_'+hemi+'_ver'+ str(n_vertex) + '_area'+ str(weight_area) + \
                                                  '_dist'+ str(weight_distan) +'_smooth'+ str(weight_smooth) +'.mdl')
         print('Loading pre-trained model:', model_file)
-        model.load_state_dict(torch.load(model_file))
+        if device == torch.device('cpu'):
+            model.load_state_dict(torch.load(model_file, map_location=torch.device('cpu')))
+        else:
+            model.load_state_dict(torch.load(model_file))
         print("{} paramerters in total".format(sum(x.numel() for x in model.parameters())))
         print('Loading pre-trained model done.')
         
@@ -129,7 +134,6 @@ if __name__ == "__main__":
         fixed_xyz = torch.from_numpy(template['vertices']).to(device) / 100.0
         bi_inter = get_bi_inter(n_vertex, device)[0]
         En = getEn(n_vertex, device)[0]
-        neighs_faces = get_neighs_faces(n_vertex)
         
         model.eval()
         for batch_idx, (inner_vert, file) in enumerate(test_dataloader):
@@ -141,7 +145,7 @@ if __name__ == "__main__":
                 inner_dist, inner_area, _ = computeMetrics_torch(inner_vert, neigh_sorted_orders, device)
                 feat = torch.cat((inner_dist, inner_area), 1)  # features
                 feat = feat.permute(1,0).unsqueeze(0)
-                deform_2d = model(feat) / deform_scale
+                deform_2d = model(feat) / 10.0
                 deform_2d = deform_2d[0,:].permute(1,0)
                 deform_3d = convert2DTo3D(deform_2d, En, device)
        
@@ -169,10 +173,15 @@ if __name__ == "__main__":
             neg_area = countNegArea(moved_surf['vertices'], moved_surf['faces'][:, 1:])
             print("Negative areas of the deformation:", neg_area)
             if save_interim_results:
+                moved_dist, moved_area, _ = computeMetrics_torch(moved_sphere_loc, neigh_sorted_orders, device)
+                loss_dist = (moved_dist - inner_dist) / (inner_dist+1e-12)
+                loss_area = (moved_area - inner_area) / (inner_area+1e-12)
+                moved_surf['dist_dis'] = loss_dist.cpu().numpy()
+                moved_surf['area_dis'] = loss_area.cpu().numpy()
                 write_vtk(moved_surf, file.replace('RespInner.vtk', 
-                                               'RespSphe.'+ str(n_vertex) +'moved.vtk'))
-        
+                                                   'RespSphe.'+ str(n_vertex) +'moved.vtk'))
 
+            
             # postprocessing
             if n_vertex == 10242:
                 orig_sphe_surf = read_vtk(file.replace('.SIP.RespInner.vtk', '.SIP.vtk'))
@@ -188,7 +197,7 @@ if __name__ == "__main__":
                 orig_inner_surf = read_vtk(file.replace('.inflated.SIP.10242moved.40962moved.RespInner.vtk', '.vtk'))
             
             print("\nMoving original sphere according to ico sphere deformation...")
-            orig_sphere_moved = moveOrigSphe(template, orig_sphe_surf, moved_surf, orig_inner_surf, neighs_faces)
+            orig_sphere_moved = moveOrigSphe(template, orig_sphe_surf, moved_surf, orig_inner_surf)
             print("Moving original sphere done!")
              
             if save_interim_results:
@@ -201,7 +210,7 @@ if __name__ == "__main__":
                 write_vtk(orig_sphere_surf, orig_sphe_moved_name)
             print("computeAndWriteDistortionOnOrigSphe done!")
             
-            if n_vertex < 163842:
+            if n_vertex < n_vertexs[-1]:
                 print("\nResampling inner surface...")
                 template_163842 = get_sphere_template(163842)
                 computeAndWriteDistortionOnRespSphe(orig_sphere_moved, template_163842, 
